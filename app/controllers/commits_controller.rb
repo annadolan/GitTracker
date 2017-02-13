b0d62e4a33565d2c6033a42c4e569a3ee6ea87dc
Anna Dolan <anna.dolan@gmail.com>
2017-02-13 10:06:37 -0700

    Update controller

--- app/controllers/commits_controller.rb
+++ app/controllers/commits_controller.rb
@@ -2,4 +2,15 @@ class CommitsController < ApplicationController
   def index
     @commits = Commit.all
   end
+
+  def create
+    data_json = JSON.parse request.body.read
+    
+    @commit = Commit.create(commit_params)
+  end
+
+  private
+    # def commit_params
+    #   params.require(:commit).permit(:sha, :message, :username, :timestamp)
+    # end
 end
