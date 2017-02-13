8a5a347b85419eacfc515480194fb55e97909e6c
Anna Dolan <anna.dolan@gmail.com>
2017-02-13 11:21:46 -0700

    stuff

--- app/controllers/commits_controller.rb
+++ app/controllers/commits_controller.rb
@@ -6,8 +6,8 @@ class CommitsController < ApplicationController
 
   def create
     # data_json = JSON.parse request.body.read
-    payload = params["payload"].to_json
-    data_json = JSON.parse(payload)
+    data_json = params["payload"].to_json
+    # data_json = JSON.parse(payload)
 
     @commit = Commit.new(sha: data_json["commits"][0]["id"], message: data_json["commits"][0]["message"], username: data_json["commits"][0]["committer"]["username"], timestamp: data_json["commits"][0]["timestamp"])
     if @commit.save
