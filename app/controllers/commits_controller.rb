14015752e3a19ab5b2d5724d8e595f2e0ec3952e
Anna Dolan <anna.dolan@gmail.com>
2017-02-13 10:33:38 -0700

    Add empty brackets

--- app/controllers/commits_controller.rb
+++ app/controllers/commits_controller.rb
@@ -7,7 +7,7 @@ class CommitsController < ApplicationController
   def create
     data_json = JSON.parse request.body.read
 
-    @commit = Commit.create(sha: data_json["payload"]["commits"]["id"], message: data_json["payload"]["commits"]["message"], username: data_json["payload"]["commits"]["committer"]["username"], timestamp: data_json["payload"]["commits"]["timestamp"])
+    @commit = Commit.create(sha: data_json["payload"]["commits"][]["id"], message: data_json["payload"]["commits"][]["message"], username: data_json["payload"]["commits"][]["committer"]["username"], timestamp: data_json["payload"]["commits"][]["timestamp"])
   end
 
   private
