53a41f77ebd56f404d1b048f154899087229c5c2
Anna Dolan <anna.dolan@gmail.com>
2017-02-13 11:03:13 -0700

    Add payload

--- app/controllers/commits_controller.rb
+++ app/controllers/commits_controller.rb
@@ -9,7 +9,7 @@ class CommitsController < ApplicationController
     json = params.to_json
     data_json = JSON.parse(json)
 
-    @commit = Commit.new(sha: data_json["commits"][0]["id"], message: data_json["commits"][0]["message"], username: data_json["commits"][0]["committer"]["username"], timestamp: data_json["commits"][0]["timestamp"])
+    @commit = Commit.new(sha: data_json["payload"]["commits"][0]["id"], message: data_json["payload"]["commits"][0]["message"], username: data_json["payload"]["commits"][0]["committer"]["username"], timestamp: data_json["payload"]["commits"][0]["timestamp"])
     if @commit.save
       render nothing: true, status: 200
     else
