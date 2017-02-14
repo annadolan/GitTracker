27837f390f44cc6948069d2d4aec6a981486c3e6
Anna Dolan <anna.dolan@gmail.com>
2017-02-14 10:26:07 -0700

    time ago

--- app/models/commit.rb
+++ app/models/commit.rb
@@ -10,4 +10,8 @@ class Commit < ApplicationRecord
   def self.today
     where('created_at >= ?', Time.zone.now.beginning_of_day).count
   end
+
+  def self.last_commit
+    last.created_at
+  end
 end
